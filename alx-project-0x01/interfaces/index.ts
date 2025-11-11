db5bbac5621d73a6d200befb854947a0e8057637
Blessed Sode <emi.square23@gmail.com>
2025-11-11 07:19:39 +0100

    added on submit function

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -82,5 +82,6 @@ export interface UserData {
 export interface UserModalProps {
   user: UserData;        // the user data being displayed in the modal
   isOpen: boolean;       // whether the modal is visible or not
+  onSubmit: (post: PostData) => void;
   onClose: () => void;   // function to close the modal
 }
